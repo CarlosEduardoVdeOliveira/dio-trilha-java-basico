fee88ff562b258bb625e7b8417ea253a8b13256c
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2024-06-18 20:18:45 -0300

    Projeto concluído

--- bootcamp/src/Main.java
+++ bootcamp/src/Main.java
@@ -1,11 +1,7 @@
 import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Curso cursoReact = new Curso();
